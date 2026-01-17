7e315baa9e6191c9b739a611851b908e52b8062e
RiseOfLegendKuN <houssemriahi007@gmail.com>
2026-01-17 12:57:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Redesigned by telegram.dog/TheFirstSpeedster at https://www.npmjs.com/package/@googledrive/index which was written by someone else, credits are given on Source Page.
-// v1.0.5
+// v1.0.6
 // Initialize the page
 function init() {
 	document.siteName = $('title').html();
